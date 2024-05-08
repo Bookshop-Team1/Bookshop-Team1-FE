9282facb81f1952ecdb41ca2efc8128cb3546446
Dilip Valiya <dilip.valiya@thoughtworks.com>
2024-05-08 13:19:58 +0530

    [Dilip] Refactor CSV Uploader component as per design

--- src/CSVUploader/CSVUploader.js
+++ src/CSVUploader/CSVUploader.js
@@ -18,39 +18,48 @@ const CSVUploader = () => {
   };
 
   return (
-    <div className={styles["upload-csv"]}>
-      <input
-        type="file"
-        accept=".csv"
-        className={styles["upload-input"]}
-        ref={inputRef}
-        onChange={handleFileChange}
-        key={Date.now()}
-        multiple
-      />
-      <button
-        type="button"
-        className={styles["login-btn"]}
-        onClick={() => inputRef.current.click()}
-      >
-        Upload CSV
+    <section className={styles["upload-csv"]}>
+      <p className={styles["load-books-text"]}>Load books to inventory</p>
+      <section className={styles["browse-section"]}>
+        <input
+          type="file"
+          accept=".csv"
+          className={styles["upload-input"]}
+          ref={inputRef}
+          onChange={handleFileChange}
+          key={Date.now()}
+          multiple
+        />
+        <p className={styles["browse-file-text"]}>Browse&nbsp;file(s)</p>
+        <img
+          src={"/addIcon.svg"}
+          alt="Browse file"
+          data-testid="testBrowseFile"
+          role="button"
+          className={styles["browse-icon"]}
+          onClick={() => inputRef.current.click()}
+        />
+        {selectedFile && (
+          <>
+            <p className={styles["csv-name"]}>
+              {selectedFile[0].name}
+              {selectedFile.length > 1 ? `, +${selectedFile.length - 1}` : ""}
+            </p>
+            <img
+              src={"/closeLineIcon.svg"}
+              alt="Remove CSV"
+              data-testid="testRemoveCSV"
+              className={styles.icon}
+              onClick={clearCSV}
+            />
+          </>
+        )}
+      </section>
+
+      <button type="button" disabled={!selectedFile} className={styles["upload-btn"]}>
+        Upload
       </button>
-      {selectedFile && (
-        <>
-          <p className={styles["csv-name"]}>
-            {selectedFile[0].name}
-            {selectedFile.length > 1 ? `, +${selectedFile.length - 1}` : ""}
-          </p>
-          <img
-            src={"/closeLineIcon.svg"}
-            alt="Remove CSV"
-            data-testid="testRemoveCSV"
-            className={styles.icon}
-            onClick={clearCSV}
-          />
-        </>
-      )}
-    </div>
+    </section>
   );
 };
 
